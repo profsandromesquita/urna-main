31f46370da31c6e4382f71b6fe2d2b255c19de11
Prof. Sandro Mesquita <62679287+profsandromesquita@users.noreply.github.com>
2023-11-11 22:40:28 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,263 @@
+let etapas = [
+    {
+        titulo: 'Heroi',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: '  BATMAN',
+                partido: 'REPUBLICANOS',
+                vice:'-----------------',
+                fotos: [
+                    {url:'herois/11111.jpg', legenda: 'Heroi'}
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'HOMEM ARANHA',
+                partido: 'PMB',
+                vice:'-----------------',
+                fotos: [
+                    {url:'herois/22222.jpg', legenda: 'Heroi'}
+                ]
+            },
+            {
+                numero: '33333',
+                nome: 'SUPER MAN',
+                partido: 'PL',
+                vice:'-----------------',
+                fotos: [
+                    {url:'herois/33333.jpg', legenda: 'Heroi'}
+                ]
+            },
+            {
+                numero: '44444',
+                nome: 'MULHER MARAVILHA',
+                partido: 'PDT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'herois/44444.jpg', legenda: 'Heroi'}
+                ]
+            },
+            {
+                numero: '55555',
+                nome: 'HOMEM DE FERRO',
+                partido: 'PT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'herois/55555.jpg', legenda: 'Heroi'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'Desenho',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '1111',
+                nome: 'PATRULHA CANINA',
+                partido: 'MDB',
+                vice:'-----------------',
+                fotos: [
+                    {url:'desenhos/1111.jpg', legenda: 'Desenho'}
+                ]
+            },
+            {
+                numero: '2222',
+                nome: 'FROZEN',
+                partido: 'REPUBLICANOS',
+                vice:'-----------------',
+                fotos: [
+                    {url:'desenhos/2222.jpg', legenda: 'Desenho'}
+                ]
+            },
+            {
+                numero: '3333',
+                nome: 'MUNDO BITA',
+                partido: 'PSOL',
+                vice:'-----------------',
+                fotos: [
+                    {url:'desenhos/3333.jpg', legenda: 'Desenho'}
+                ]
+            },
+            {
+                numero: '4444',
+                nome: 'BEBÊ TUBARÃO',
+                partido: 'PDT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'desenhos/4444.jpg', legenda: 'Desenho'}
+                ]
+            },
+            {
+                numero: '5555',
+                nome: 'ANJINHOS',
+                partido: 'PT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'desenhos/5555.jpg', legenda: 'Desenho'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'Lanche',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '111',
+                nome: 'FRUTA',
+                partido: 'PT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'lanches/111.jpg', legenda: 'Lanche'}
+                ]
+            },
+            {
+                numero: '222',
+                nome: 'BISCOITO',
+                partido: 'PSTU',
+                vice:'-----------------',
+                fotos: [
+                    {url:'lanches/222.jpg', legenda: 'Lanche'}
+                ]
+            },
+            {
+                numero: '333',
+                nome: 'XILITO',
+                partido: 'AVANTE',
+                vice:'-----------------',
+                fotos: [
+                    {url:'lanches/333.jpg', legenda: 'Lanche'}
+                ]
+            },
+            {
+                numero: '444',
+                nome: 'SOPINHA',
+                partido: 'PSD',
+                vice:'-----------------',
+                fotos: [
+                    {url:'lanches/444.jpg', legenda: 'Lanche'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'Brinquedo',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'JOGO',
+                partido: 'UNIÃO BRASIL',
+                vice:'QUEBRA CABEÇA',
+                fotos: [
+                    {url:'brinquedos/11.jpg', legenda: 'Brinquedo'},
+                    {url:'brinquedos/vice11.jpg', legenda: 'Vice-Brinquedo'}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'BOLA',
+                partido: 'PCB',
+                vice:'BILA',
+                fotos: [
+                    {url:'brinquedos/22.jpg', legenda: 'Brinquedo'},
+                    {url:'brinquedos/vice22.jpg', legenda: 'Vice-Brinquedo'}
+                ]
+            },
+            {
+                numero: '33',
+                nome: 'BARBIE',
+                partido: 'PT',
+                vice:'URSO',
+                fotos: [
+                    {url:'brinquedos/33.jpg', legenda: 'Brinquedo'},
+                    {url:'brinquedos/vice33.jpg', legenda: 'Vice-Brinquedo'}
+                ]
+            },
+            {
+                numero: '44',
+                nome: 'MCQUEEN',
+                partido: 'PSD',
+                vice:'MATE',
+                fotos: [
+                    {url:'brinquedos/44.jpg', legenda: 'Brinquedo'},
+                    {url:'brinquedos/vice44.jpg', legenda: 'Vice-Brinquedo'}
+                ]
+            },
+            {
+                numero: '55',
+                nome: 'CASINHA',
+                partido: 'UP',
+                vice:'PANELINHAS',
+                fotos: [
+                    {url:'brinquedos/55.jpg', legenda: 'Brinquedo'},
+                    {url:'brinquedos/vice55.jpg', legenda: 'Vice-Brinquedo'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'TIA',
+        numeros: 2,
+        candidatos:[
+            {
+                numero:'66',
+                nome:'TIA DAYSE',
+                partido:'PL',
+                vice:'COLÉGIO LUMEN BABY',
+                fotos:[
+                    {url:'tias/66.jpg', legenda: 'Tia Dayse'},
+                    {url:'tias/vice.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero:'77',
+                nome:'TIA KARINNE',
+                partido:'PT',
+                vice:'COLÉGIO LUMEN BABY',
+                fotos:[
+                    {url:'tias/77.jpg', legenda: 'Tia Karine'},
+                    {url:'tias/vice.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero:'88',
+                nome:'TIA LEILA',
+                partido:'UNIÃO',
+                vice:'COLÉGIO LUMEN BABY',
+                fotos:[
+                    {url:'tias/88.jpg', legenda: 'Tia Leila'},
+                    {url:'tias/vice.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero:'99',
+                nome:'TIA MARCIA',
+                partido:'PTB',
+                vice:'COLÉGIO LUMEN BABY',
+                fotos:[
+                    {url:'tias/99.jpg', legenda: 'Tia Marcia'},
+                    {url:'tias/vice.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero:'00',
+                nome:'TIO DO KARATÊ',
+                partido:'PDT',
+                vice:'COLÉGIO LUMEN BABY',
+                fotos:[
+                    {url:'tias/00.jpg', legenda: 'Tio do Karatê'},
+                    {url:'tias/vice.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+
+        ]
+    }
+];
